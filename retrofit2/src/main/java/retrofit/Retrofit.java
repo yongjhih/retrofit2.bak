d0aa46550c85cd3457c51545ebf13dca91504371
Andrew Chen <yongjhih@gmail.com>
2015-06-27 14:04:33 +0800

    init

---
+++ retrofit2/src/main/java/retrofit/Retrofit.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2012 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package retrofit;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Specifies that <a href="https://github.com/frankiesardo/retrofit">Retrofit</a> should
+ * generate an implementation class for the annotated abstract class, implementing the standard
+ * {@link Object} methods like {@link Object#equals equals} to have conventional value semantics. A
+ * simple example: <pre>
+ *
+ *   &#64;Retrofit
+ *   abstract class Person implements Parcelable {
+ *     static Person create(String name, int id) {
+ *       return new Retrofit_Person(name, id);
+ *     }
+ *
+ *     abstract String name();
+ *     abstract int id();
+ *   }</pre>
+ *
+ * @author Éamonn McManus
+ * @author Kevin Bourrillion
+ * @see <a href="https://github.com/frankiesardo/retrofit">Retrofit User's Guide</a>
+ */
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.TYPE)
+public @interface Retrofit {
+
+  /**
+   * Specifies that Retrofit should generate an implementation of the annotated class or interface,
+   * to serve as a <i>builder</i> for the value-type class it is nested within. As a simple example,
+   * here is an alternative way to write the {@code Person} class mentioned in the {@link Retrofit}
+   * example: <pre>
+   *
+   *   &#64;Retrofit
+   *   abstract class Person {
+   *     static Builder builder() {
+   *       return new Retrofit_Person.Builder();
+   *     }
+   *
+   *     abstract String name();
+   *     abstract int id();
+   *
+   *     &#64;Retrofit.Builder
+   *     interface Builder {
+   *       Builder name(String x);
+   *       Builder id(int x);
+   *       Person build();
+   *     }
+   *   }</pre>
+   *
+   * <p><b>This API is provisional and subject to change.</b></p>
+   *
+   * @author Éamonn McManus
+   */
+  @Retention(RetentionPolicy.SOURCE)
+  @Target(ElementType.TYPE)
+  public @interface Builder {
+  }
+
+  /**
+   * Specifies that the annotated method is a validation method. The method should be a non-private
+   * no-argument method in an Retrofit class. It will be called by the {@code build()} method of
+   * the {@link Builder @Retrofit.Builder} implementation, immediately after constructing the new
+   * object. It can throw an exception if the new object fails validation checks.
+   */
+  @Retention(RetentionPolicy.SOURCE)
+  @Target(ElementType.METHOD)
+  public @interface Validate {
+  }
+}
