51d41f7ba0b68652c2384f5dd5a6dd20176e1039
Andrew Chen <yongjhih@gmail.com>
2015-07-23 16:10:34 +0800

    github: More fields

--- retrofit2-github/src/main/java/com/github/retrofit2/Contributor.java
+++ retrofit2-github/src/main/java/com/github/retrofit2/Contributor.java
@@ -27,6 +27,39 @@ public class Contributor {
     @JsonField
     public int contributions;
 
+    @JsonField
+    public long id;
+    @JsonField
+    public String avatar_url;
+    @JsonField
+    public String gravatar_id;
+    @JsonField
+    public String url;
+    @JsonField
+    public String html_url;
+    @JsonField
+    public String followers_url;
+    @JsonField
+    public String following_url;
+    @JsonField
+    public String gists_url;
+    @JsonField
+    public String starred_url;
+    @JsonField
+    public String subscriptions_url;
+    @JsonField
+    public String organizations_url;
+    @JsonField
+    public String repos_url;
+    @JsonField
+    public String events_url;
+    @JsonField
+    public String received_events_url;
+    @JsonField
+    public String type;
+    @JsonField
+    public boolean site_admin;
+
     //@JsonIgnore
     //public String login() {
         //return login();
--- retrofit2-github/src/main/java/com/github/retrofit2/Repository.java
+++ retrofit2-github/src/main/java/com/github/retrofit2/Repository.java
@@ -19,23 +19,144 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.bluelinelabs.logansquare.annotation.JsonField;
 import com.bluelinelabs.logansquare.annotation.JsonObject;
 
+/**
+ * <pre>
+ *  "id": 1296269,
+ *  "owner": {
+ *    "login": "octocat",
+ *    "id": 1,
+ *    "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+ *    "gravatar_id": "",
+ *    "url": "https://api.github.com/users/octocat",
+ *    "html_url": "https://github.com/octocat",
+ *    "followers_url": "https://api.github.com/users/octocat/followers",
+ *    "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+ *    "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+ *    "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+ *    "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+ *    "organizations_url": "https://api.github.com/users/octocat/orgs",
+ *    "repos_url": "https://api.github.com/users/octocat/repos",
+ *    "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+ *    "received_events_url": "https://api.github.com/users/octocat/received_events",
+ *    "type": "User",
+ *    "site_admin": false
+ *  },
+ *  "name": "Hello-World",
+ *  "full_name": "octocat/Hello-World",
+ *  "description": "This your first repo!",
+ *  "private": false,
+ *  "fork": true,
+ *  "url": "https://api.github.com/repos/octocat/Hello-World",
+ *  "html_url": "https://github.com/octocat/Hello-World",
+ *  "clone_url": "https://github.com/octocat/Hello-World.git",
+ *  "git_url": "git://github.com/octocat/Hello-World.git",
+ *  "ssh_url": "git@github.com:octocat/Hello-World.git",
+ *  "svn_url": "https://svn.github.com/octocat/Hello-World",
+ *  "mirror_url": "git://git.example.com/octocat/Hello-World",
+ *  "homepage": "https://github.com",
+ *  "language": null,
+ *  "forks_count": 9,
+ *  "stargazers_count": 80,
+ *  "watchers_count": 80,
+ *  "size": 108,
+ *  "default_branch": "master",
+ *  "open_issues_count": 0,
+ *  "has_issues": true,
+ *  "has_wiki": true,
+ *  "has_pages": false,
+ *  "has_downloads": true,
+ *  "pushed_at": "2011-01-26T19:06:43Z",
+ *  "created_at": "2011-01-26T19:01:12Z",
+ *  "updated_at": "2011-01-26T19:14:43Z",
+ *  "permissions": {
+ *    "admin": false,
+ *    "push": false,
+ *    "pull": true
+ *  }
+ * <pre>
+ *
+ * @see "https://developer.github.com/v3/repos/"
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonObject
 public class Repository {
     @JsonField
-    public String login;
+    public long id;
     @JsonField
-    public int contributions;
+    public Contributor owner;
+    @JsonField
+    public String name;
+    @JsonField
+    public String full_name;
+    @JsonField
+    public String description;
 
-    //@JsonIgnore
-    //public String login() {
-        //return login();
-    //}
-    public Repository() {
+    @JsonField(name = "private")
+    public boolean isPrivate;
+
+    @JsonField
+    public boolean fork;
+    @JsonField
+    public String url;
+    @JsonField
+    public String html_url;
+    @JsonField
+    public String clone_url;
+    @JsonField
+    public String git_url;
+    @JsonField
+    public String ssh_url;
+    @JsonField
+    public String svn_url;
+    @JsonField
+    public String mirror_url;
+    @JsonField
+    public String homepage;
+    @JsonField
+    public String language;
+    @JsonField
+    public int forks_count;
+    @JsonField
+    public int stargazers_count;
+    @JsonField
+    public int watchers_count;
+    @JsonField
+    public int size;
+    @JsonField
+    public String default_branch;
+    @JsonField
+    public int open_issues_count;
+    @JsonField
+    public boolean has_issues;
+    @JsonField
+    public boolean has_wiki;
+    @JsonField
+    public boolean has_pages;
+    @JsonField
+    public boolean has_downloads;
+    @JsonField
+    public String pushed_at;
+    @JsonField
+    public String created_at;
+    @JsonField
+    public String updated_at;
+    @JsonField
+    public Permission permissions;
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @JsonObject
+    public static class Permission {
+        @JsonField
+        public boolean admin;
+        @JsonField
+        public boolean push;
+        @JsonField
+        public boolean pull;
+
+        public Permission() {
+        }
     }
 
-    public Repository(String login, int contributions) {
-        this.login = login;
-        this.contributions = contributions;
+    public Repository() {
     }
 }
